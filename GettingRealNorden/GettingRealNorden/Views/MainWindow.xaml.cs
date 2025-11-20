a154a6b537cfa7d1d2f16d28b3c3d4bfafe4bfe3
Jeppe <jeppe@helsted.com>
2025-11-20 08:32:12 +0100

    GUI for FLOWLETTER er begyndt og der implementeret en smule

--- GettingRealNorden/GettingRealNorden/Views/MainWindow.xaml.cs
+++ GettingRealNorden/GettingRealNorden/Views/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using GettingRealNorden.Views;
 
 namespace GettingRealNorden
 {
@@ -19,6 +20,28 @@ namespace GettingRealNorden
         public MainWindow()
         {
             InitializeComponent();
+            btn_Newsletter.IsEnabled = false;
+        }
+
+        private void btn_Members_Click(object sender, RoutedEventArgs e)
+        {
+             
+        }
+
+        private void btn_Newsletter_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_CreateNewsletterDialog_Click(object sender, RoutedEventArgs e)
+        {
+            CreateNewsletterDialog createNewsletterDialog = new CreateNewsletterDialog();
+            this.Hide(); //Hides MainWindow when btn_CreateNewsletterDialog_Click is initialized
+            if (createNewsletterDialog.ShowDialog() == false) //If the Newsletter dialog is closed/false it shows the main window again
+            {
+                this.Show(); //this refers to the active window/class
+                
+            }
         }
     }
 }
