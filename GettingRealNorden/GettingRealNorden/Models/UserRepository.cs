7f4da33e18e8ecab5c179b3b014bc031d1cfc4d2
Mathias <mathiasohrtkrogh@gmail.com>
2025-11-24 14:30:07 +0100

    Add from file

--- GettingRealNorden/GettingRealNorden/Models/UserRepository.cs
+++ GettingRealNorden/GettingRealNorden/Models/UserRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +10,33 @@ namespace GettingRealNorden.Models
     public class UserRepository
     {
         private List<Newsletter> newsletters;
+        private void InitializeRepository()
+        {
+            try
+            {   // Open the text file using a stream reader.
+                using (StreamReader sr = new StreamReader("Persons.csv"))
+                {
+                    // Read the stream to a string, and instantiate a Person object
+                    string line = sr.ReadLine();
+
+                    while (line != null)
+                    {
+                        string[] parts = line.Split(',');
+
+                        // parts[0] contains first name, parts[1] contains last name, parts[2] contains age as text, parts[3] contains phone
+
+                        this.Add(parts[0], parts[1], int.Parse(parts[2]), parts[3]);
+
+                        //Read the next line
+                        line = sr.ReadLine();
+                    }
+                }
+            }
+            catch (IOException)
+            {
+                throw;
+            }
+        }
+        public
     }
 }
